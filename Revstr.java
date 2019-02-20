2c8debb5bc9b7f38a3b84216b5f7585454fd21c0
safaranees <aneesparvin08@gmail.com>
2019-02-20 14:17:04 +0530

    Update Revstr.java

--- Revstr.java
+++ Revstr.java
@@ -5,7 +5,9 @@ public class Revstr
 {
         public static void main(String args[])
         {
-                StringBuffer  revnum = new StringBuffer("aabbcc");
+                Scanner s=new Scanner(System.in);
+                String str=s.nextLine();
+                StringBuffer  revnum = new StringBuffer(s);
                 System.out.println(revnum.reverse());
         }
 }
