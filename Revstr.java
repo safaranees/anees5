9852671c64f0188e68ef3fb72590625f072b0d90
safaranees <aneesparvin08@gmail.com>
2019-02-20 14:11:10 +0530

    Create Revstr.java

---
+++ Revstr.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+public class Revstr
+{
+        public static void main(String args[])
+        {
+                StringBuffer  revnum=StringBuffer("aabbcc");
+                System.out.println(a.reverse());
+        }
+}
