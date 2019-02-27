9dd169449ab21d2d03dd51045971f0b727663095
safaranees <aneesparvin08@gmail.com>
2019-02-27 14:27:06 +0530

    Update CountWord.java

--- CountWord.java
+++ CountWord.java
@@ -7,7 +7,7 @@ class CountWord
     Scanner Word=new Scanner(System.in);
     String str=Word.next();
     int count=1;
-    for(i=0;i<s.length-1;i++)
+    for(int i=0;i<s.length-1;i++)
     {
       if((str.charAt(i)==' ')&&(str.charAt(i+1)!=' '))
       {
